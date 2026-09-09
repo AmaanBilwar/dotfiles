4c0520d2d8531996c8fb80595108134451eec6be
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-09-09 07:14:19 +0000

    add caveman to nix conifg

--- modules/packages.nix
+++ modules/packages.nix
@@ -22,6 +22,15 @@
     pkgs.age
     pkgs.sops
 
+    # Caveman installer. Run `caveman-install` once after switching.
+    (pkgs.writeShellApplication {
+      name = "caveman-install";
+      runtimeInputs = [ pkgs.bash pkgs.curl pkgs.nodejs_22 ];
+      text = ''
+        exec curl -fsSL https://raw.githubusercontent.com/JuliusBrussee/caveman/v2.6.0/install.sh | bash
+      '';
+    })
+
     # Desktop and hardware configuration tools.
     pkgs.zed-editor
     pkgs.kanata
